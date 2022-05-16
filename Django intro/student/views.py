a8de7f76ec979c4d0b92d8c8db69e6608ed6ec5a
muratgcmz <gocmezmurat@gmail.com>
2022-05-16 19:46:44 +0300

    session started

---
+++ Django intro/student/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello world...")
+
+def home(request):
+    return HttpResponse("My name is Henry")    
