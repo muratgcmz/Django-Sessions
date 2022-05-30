db8340af3f645a473a11506da68ef4d38c13d836
muratgcmz <gocmezmurat@gmail.com>
2022-05-30 20:19:17 +0300

    decouple fixed

--- Auth_2_Extending_User/main/settings.py
+++ Auth_2_Extending_User/main/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-# from decouple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cms1i%=07=(q8n*n044t1)u(*y$mp&ec%u(2eqa=%of9xu**s-'
-# SECRET_KEY = config('SECRET_KEY')
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
