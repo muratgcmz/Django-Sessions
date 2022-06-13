771180910705878b63c3c9f61f6b6214dcc14a58
muratgcmz <gocmezmurat@gmail.com>
2022-06-13 20:08:13 +0300

    done

--- 020_DRF_Serializers/main/settings.py
+++ 020_DRF_Serializers/main/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'student_api'
+    'student_api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- 020_DRF_Serializers/student_api/models.py
+++ 020_DRF_Serializers/student_api/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+
+class Path(models.Model):
+    path_name = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.path_name
 class Student(models.Model): 
     first_name = models.CharField(max_length=30) 
     last_name = models.CharField(max_length=30) 
-    number = models.IntegerField(blank=True, null=True) 
+    number = models.IntegerField(blank=True, null=True)
+    register_date = models.DateTimeField(auto_now_add=True) 
     # blank=True for admin dashboard 
     # null=True for db 
  
